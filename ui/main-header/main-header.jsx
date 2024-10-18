3ce1177097a065e82d7731d8a28b34028d17d806
Ric Mershon <rmersh@ricmershon.com>
2024-10-18 13:58:52 -0400

    Client component for main header links

--- ui/main-header/main-header.jsx
+++ ui/main-header/main-header.jsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 
+import NavLink from './nav-link';
+
 export default function MainHeader() {
-  return (
+    return (
         <header id="main-header">
             <div id="logo">
                 <Link href="/">NextNews</Link>
@@ -9,8 +11,10 @@ export default function MainHeader() {
             <nav>
                 <ul>
                     <li>
-                        <Link href="/news">News</Link>
-                        <Link href="/archive">Archive</Link>
+                        <NavLink href='/news'>News</NavLink>
+                    </li>
+                    <li>
+                        <NavLink href='/archive'>Archive</NavLink>
                     </li>
                 </ul>
             </nav>
