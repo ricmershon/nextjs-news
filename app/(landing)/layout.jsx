16b59bfe2166f973cdf8b1561acda8711dec8e13
Ric Mershon <rmersh@ricmershon.com>
2024-10-18 16:09:48 -0400

    Landing page and route groups

---
+++ app/(landing)/layout.jsx
@@ -0,0 +1,12 @@
+import '../globals.css';
+
+export const metadata = {
+    title: 'Next.js Page Routing & Rendering',
+    description: 'Learn how to route to different pages.',
+}
+
+export default function LandingLayout({ children }) {
+    return (
+        <main>{children}</main>
+    )
+}
