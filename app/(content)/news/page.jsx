fc6d47f85e9b39894dc62d74bea67b8cd1068dab
Ric Mershon <rmersh@ricmershon.com>
2024-10-18 17:04:42 -0400

    Remove backend and access database directly from server

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,13 +1,8 @@
+import { getAllNews } from "@/lib/news";
 import NewsList from "@/ui/news-list/news-list";
 
 export default async function NewsPage() {
-    const response = await fetch('http://localhost:8080/news');
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch news')
-    }
-
-    const news = await response.json();
+    const news = getAllNews();
 
     return (
         <>
