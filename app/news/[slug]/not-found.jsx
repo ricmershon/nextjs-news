deceee5e8f8a6a180adb2906e78c8dbb4c587836
Ric Mershon <rmersh@ricmershon.com>
2024-10-18 11:26:08 -0400

    Not found pages for app and news articles

---
+++ app/news/[slug]/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NewsNotFoundPage() {
+    return (
+        <div id='error'>
+            <h1>Not Found.</h1>
+            <p>News article not found.</p>
+        </div>
+    )
+}
