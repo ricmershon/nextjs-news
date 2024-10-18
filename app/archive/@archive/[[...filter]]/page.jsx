be869af6dc8eaf9686fee5e904980a3821426118
Ric Mershon <rmersh@ricmershon.com>
2024-10-18 12:39:08 -0400

    Catch all page for years and months

---
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import NewsList from "@/ui/news-list/news-list";
+
+export default function FilteredNewsPage({params}) {
+    const filter = params.filter;
+
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
+
+    let news;
+    let links = getAvailableNewsYears();
+
+    if (selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    if (selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = [];
+    }
+
+    let newsContent = <p>No news available for the slected period.</p>
+
+    if (news && news.length > 0) {
+        newsContent = <NewsList news={news} />
+    }
+
+    return (
+        <>
+            <header id='archive-header'>
+                <nav>
+                    <ul>
+                        {links.map((link) => {
+                            const href = selectedYear
+                                ? `/archive/${selectedYear}/${link}`
+                                : `/archive/${link}`;
+
+                            return (
+                                <li key={link}>
+                                    <Link href={href}>{link}</Link>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </nav>
+            </header>
+            {newsContent}
+        </>
+    )
+}
--- app/archive/@archive/[year]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { getNewsForYear } from "@/lib/news";
-import NewsList from "@/ui/news-list/news-list";
-
-export default function FilteredNewsPage({params}) {
-    const news = getNewsForYear(params.year)
-    return (
-        <NewsList news={news} />
-    )
-}
